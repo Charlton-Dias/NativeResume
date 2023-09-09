f6c8e666aa757e09b6997d4e2c7fc38e5bbe3643
Charlton Dias <charltondias29@gmail.com>
2023-09-09 10:56:48 +0530

    added image

--- App.tsx
+++ App.tsx
@@ -4,13 +4,15 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
+  Image,
   useColorScheme,
   View,
   Linking,
   FlatList,
 } from 'react-native';
+
+import styles from './styles';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
@@ -95,6 +97,8 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+            
+          <Image source={require('./src/avatar.jpeg')} style={styles.image}/>
 
           <Text style={styles.title}>
             Charlton Dias
@@ -163,58 +167,6 @@ function App(): JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    padding: 24,
-    backgroundColor: '#202020',
-    borderRadius: 10,
-    margin: 10,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    marginTop: 10,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  educationItem: {
-    width: '90%',
-    marginBottom: 20,
-    padding: 16,
-    backgroundColor: '#303030',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#353535',
-  },
-  section: {
-    width: '95%',
-    padding: 2,
-    borderWidth: 1,
-    backgroundColor: '#555555',
-    borderRadius: 8,
-  },
-  tags: {
-    fontSize: 18,
-    padding: 8,
-    margin: 4,
-    backgroundColor: '#C2A515',
-    borderRadius: 10,
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: 46,
-    textAlign: 'center',
-    marginTop: 20,
-    color: '#F2C515',
-  }
-});
 
 
 
---
+++ styles.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    padding: 24,
+    backgroundColor: '#202020',
+    borderRadius: 10,
+    margin: 10,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: 10,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  educationItem: {
+    width: '90%',
+    marginBottom: 20,
+    padding: 16,
+    backgroundColor: '#303030',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#353535',
+  },
+  section: {
+    width: '95%',
+    padding: 2,
+    borderWidth: 1,
+    backgroundColor: '#555555',
+    borderRadius: 8,
+  },
+  tags: {
+    fontSize: 18,
+    padding: 8,
+    margin: 4,
+    backgroundColor: '#C2A515',
+    borderRadius: 10,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 46,
+    textAlign: 'center',
+    marginTop: 20,
+    color: '#F2C515',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 30,
+    alignSelf: 'center',
+    marginTop: 20,
+  }
+});
